908121370154d18bc27c1b0a61409871d2b226ae
Ana Bento <95715418+anabent0@users.noreply.github.com>
2022-11-12 16:54:31 -0300

    Componentização Perguntas e Opções Quiz

---
+++ Client/secundary/components/PerguntasQuiz.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function PerguntasQuiz() {
+    return
+    <div>
+        <div className="container" id="title">
+            <h2>Conte-nos o que achou!</h2>
+        </div>
+        <div className="container" id="question">
+            <p>Pergunta 1</p>
+        </div>
+    </div>;
+}
+
+export default PerguntasQuiz;
