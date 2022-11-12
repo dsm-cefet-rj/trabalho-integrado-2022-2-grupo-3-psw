bb26ada986b2f0f5b0097f2193c8405d77fa9d7a
Ana Bento <95715418+anabent0@users.noreply.github.com>
2022-11-12 18:42:06 -0300

    Style de componentes botão enviar, anterior e footer

--- Client/deepsleep/src/components/BotaoAnteriorQuiz.jsx
+++ Client/deepsleep/src/components/BotaoAnteriorQuiz.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./styleBotaoAnterior.css"
 function BotaoAnteriorQuiz() {
     return(
     <div>
--- Client/deepsleep/src/components/Footer.jsx
+++ Client/deepsleep/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styleFooter.css";
 
 function Footer() {
     return(
