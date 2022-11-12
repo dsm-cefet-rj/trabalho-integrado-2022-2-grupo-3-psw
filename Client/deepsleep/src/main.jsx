cdf306ddb1efa6667a510df4526f5c13ef500f55
isdavidd <vini1100@outlook.com>
2022-11-12 19:00:24 -0300

    aaaa

--- Client/deepsleep/src/main.jsx
+++ Client/deepsleep/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
