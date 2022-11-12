4ba01e11a90c1124cbdfb1b5464b70751614ad99
Ana Bento <95715418+anabent0@users.noreply.github.com>
2022-11-12 17:14:33 -0300

    Componentização Footer

---
+++ Client/secundary/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return
+    <div>
+        <div id="copyArea">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>Desenvolvido por <a>Os crias</a> &copy;2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
+}
+
+export default Footer;
