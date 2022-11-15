f3e8fe2415a51c5e4a3a819f4cb1a858a723eb59
Ana Luiza Seabra Ventapane Soares <analuizaventapane@gmail.com>
2022-11-15 03:30:52 -0300

    Componentização do Quiz Recomendação Inicio

--- Client/src/pages/Quizes/QuizRecomendacaoInicio/RecomendacaoInicio.jsx
+++ Client/src/pages/Quizes/QuizRecomendacaoInicio/RecomendacaoInicio.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import NavbarComp from "../../../components/NavbarComp";
 import ImportaBootstrap from "../../../components/ImportsBootStrap";
+import Container from "react-bootstrap/esm/Container";
+import Perguntas from "../Perguntas";
+import OpcoesQuiz from "../OpcoesQuiz";
+import BotaoEnviarQuiz from "../BotaoEnviarQuiz";
+import foto from "../../../img/bro.png";
 
 
 function RecomendacaoInicio() {
@@ -8,8 +13,20 @@ function RecomendacaoInicio() {
 
     return (
         <div>
+            <ImportaBootstrap />
 
-
+            <body>
+                <NavbarComp />
+                <Container>
+                    <h2>Bem vindo ao quiz do sono!</h2>
+                </Container>
+                <Perguntas/>
+                <OpcoesQuiz/>
+                <BotaoEnviarQuiz/>
+                <div id="image">
+                    <img src={foto}/>
+                </div>
+            </body>
         </div>
 
     );
