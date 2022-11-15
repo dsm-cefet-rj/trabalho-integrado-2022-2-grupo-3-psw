e38488cb61a91d0706a7ab51774dcf7cf7c6afdc
Ana Luiza Seabra Ventapane Soares <analuizaventapane@gmail.com>
2022-11-15 03:20:22 -0300

    Página de Quiz de Recomendação adicionada de fato

---
+++ Client/src/pages/Quizes/QuizRecomendacaoInicio/RecomendacaoInicio.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarComp from "../../../components/NavbarComp";
+import ImportaBootstrap from "../../../components/ImportsBootStrap";
+
+
+function RecomendacaoInicio() {
+
+
+    return (
+        <div>
+
+
+        </div>
+
+    );
+}
+export default RecomendacaoInicio;
