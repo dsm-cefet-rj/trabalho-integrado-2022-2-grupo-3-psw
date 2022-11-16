07b812d4cc5d7a715b0fde03ec1611cf9506f8df
Nul0 <danielpedro7676@gmail.com>
2022-11-15 21:33:53 -0300

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,4 @@
 import React from "react"
-import LoginPage from "./pages/Login"
-import RegisterPage from "./pages/Cadastro"
-import Home from "./pages/Home"
-import CartPage from "./pages/Carrinho"
-import Catalogo from "./pages/Catalogo"
-import Detalhes from "./pages/Detalhes"
-import SatisfacaoPageI from "./pages/Quizes/QuizSatisfacaoInicial/SatisfacaoPageI"
-import SatisfacaoPageM from "./pages/Quizes/QuizSatisfacaoM/SatisfacaoPageM"
-import SatisfacaoPageF from "./pages/Quizes/QuizSatisfacaoFinal/SatisfacaoPageF"
-import UserPage from "./pages/UserSpace"
-import AcompanharPedido from "./pages/AcompanharPedido"
 
 import { AppRoutes } from "./routes/Routes"
 function App() {
