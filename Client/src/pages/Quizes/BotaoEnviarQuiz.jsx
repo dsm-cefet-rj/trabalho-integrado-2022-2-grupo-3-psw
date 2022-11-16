4f41e6c4e151bbc1a053d5e7494f0cc1d6800120
Ana Luiza Seabra Ventapane Soares <analuizaventapane@gmail.com>
2022-11-16 01:47:01 -0300

    Opcoes do tipo Radio e botão enviar centralizado

--- Client/src/pages/Quizes/BotaoEnviarQuiz.jsx
+++ Client/src/pages/Quizes/BotaoEnviarQuiz.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./styleBotaoEnviar.css";
+import { Link } from "react-router-dom";
 
 function BotaoEnviarQuiz() {
-    return(
-    <div>
-        <div className="container">
-            <a class="text-decoration-none">
-                <button id="enviar">
-                    <p>Enviar</p>
-                </button>
-            </a>
-        </div>
-    </div >)
+    return (
+        <div>
+            <div className="container" id="botaoEnviar">
+                <Link to={"/catalogue"}>
+                    <button id="enviar">
+                        <p>Enviar</p>
+                    </button>
+                </Link>
+
+            </div>
+        </div >)
 }
 
 export default BotaoEnviarQuiz;
