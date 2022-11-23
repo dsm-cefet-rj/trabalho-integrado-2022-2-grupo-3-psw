1f921fa7da16b0dc624561294d424678f5111e8c
Nul0 <danielpedro7676@gmail.com>
2022-11-23 08:31:53 -0300

    Create useApi.js

---
+++ Client/src/Hooks/useApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000"
+})
+
+export const useApi = () => ({
+    calcularValorFrete: async (cep) => {
+        const response = await api.post("/cep/" + cep)
+        return response.data;
+    }
+})
