758b0e91e0fe5523d1967204521f703f777af2de
Ana Bento <95715418+anabent0@users.noreply.github.com>
2022-11-30 09:22:49 -0300

    Quiz Satisfação adicionado ao Server

---
+++ Server/quizSatisfacao.js
@@ -0,0 +1,104 @@
+const quizSatis = [
+    {
+        "idQuiz": "1",
+        "questoes": {
+            "q1": {
+                "perguntaSatisfacao": "Você está satisfeito com o produto comprado?",
+                "opcoesSatisfacao": {
+                    "A": "Muito satisfeito",
+                    "B": "Satisfeito",
+                    "C": "Pouco satisfeito",
+                    "D": "Insatisfeito"
+                }
+            },
+            "q2": {
+                "perguntaSatisfacao": "Você compraria de novo?",
+                "opcoesSatisfacao": {
+                    "A": "Com certeza",
+                    "B": "Talvez",
+                    "C": "Com outro fornecedor",
+                    "D": "Não"
+                }
+            },
+            "q3": {
+                "perguntaSatisfacao": "Você recomendaria produtos do mesmo fornecedor para outra pessoa?",
+                "opcoesSatisfacao": {
+                    "A": "Sim",
+                    "B": "Talvez",
+                    "C": "Não recomendaria",
+                    "D": "Desconheço outro produto"
+                }
+            },
+            "q4": {
+                "perguntaSatisfacao": "Como avalia a velocidade de entrega do produto?",
+                "opcoesSatisfacao": {
+                    "A": "Muito boa",
+                    "B": "Boa",
+                    "C": "Poderia ser melhor",
+                    "D": "Péssima"
+                }
+            },
+            "q5": {
+                "perguntaSatisfacao": "Como avalia o fornecedor?",
+                "opcoesSatisfacao": {
+                    "A": "Muito bom",
+                    "B": "Bom",
+                    "C": "Poderia ser melhor",
+                    "D": "Péssimo"
+                }
+            }
+        }
+    },
+    {
+        "idQuiz": "2",
+        "questoes": {
+            "q1": {
+                "perguntaSatisfacao": "O produto comprado atendeu as suas necessidades?",
+                "opcoesSatisfacao": {
+                    "A": "Sim",
+                    "B": "Um pouco",
+                    "C": "Não tanto",
+                    "D": "Não"
+                }
+            },
+            "q2": {
+                "perguntaSatisfacao": "Você compraria de novo?",
+                "opcoesSatisfacao": {
+                    "A": "Com certeza",
+                    "B": "Talvez",
+                    "C": "Com outro fornecedor",
+                    "D": "Não"
+                }
+            },
+            "q3": {
+                "perguntaSatisfacao": "Você comparia produtos do mesmo fornecedor para outra pessoa?",
+                "opcoesSatisfacao": {
+                    "A": "Sim",
+                    "B": "Talvez",
+                    "C": "Não",
+                    "D": "Desconheço outros produtos"
+                }
+            },
+            "q4": {
+                "perguntaSatisfacao": "O produto chegou em que condição?",
+                "opcoesSatisfacao": {
+                    "A": "Ótima condição",
+                    "B": "Boa condição",
+                    "C": "Um pouco danificado",
+                    "D": "Péssima condição"
+                }
+            },
+            "q5": {
+                "perguntaSatisfacao": "Como avalia o fornecedor?",
+                "opcoesSatisfacao": {
+                    "A": "Muito bom",
+                    "B": "Bom",
+                    "C": "Poderia ser melhor",
+                    "D": "Péssimo"
+                }
+            }
+        }
+    }
+]
+
+export default quizSatis;
